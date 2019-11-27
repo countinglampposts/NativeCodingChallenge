af37dfa82e78ec3e92d8334dae04346430f09c0b
Chris <countinglampposts@gmail.com>
2019-11-26 18:13:58 -0700

    Added LinqUtils which contains a method that can filter a list to its even values

---
+++ NativeCodingChallenge/LinqUtils.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NativeCodingChallenge
+{
+    public static class LinqUtils
+    {
+        public static IEnumerable<int> FilterEven(this IEnumerable<int> enumerable)
+        {
+            return enumerable
+                .Where(n => n % 2 == 0);
+        }
+    }
+}
